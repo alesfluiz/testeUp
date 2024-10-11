46de34c2166aea5d30c0755eb5ead21575ea8609
alesfluiz <alesfluiz@gmail.com>
2024-10-11 00:29:16 -0300

    update

--- Testes/Program.cs
+++ Testes/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq.Expressions;
+using System.Security.Principal;
 
 namespace Testes
 {
@@ -9,6 +10,34 @@ namespace Testes
         static void Main(string[] args)
         {
 
+            string[] peca1 = Console.ReadLine().Split(' ');
+
+            int codigo = int.Parse(peca1[0]);
+            int quantidade = int.Parse(peca1[1]);
+            double valor = double.Parse(peca1[2], CultureInfo.InvariantCulture);
+            double valorP1 = quantidade * valor;
+
+            string[] peca2 = Console.ReadLine().Split(' ');
+
+            int codigo2 = int.Parse(peca2[0]);
+            int quantidade2 = int.Parse(peca2[1]);
+            double valor2 = double.Parse(peca2[2],CultureInfo.InvariantCulture);
+            double valorP2 = quantidade2 * valor2;
+
+            double total = valor + valorP2;
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+
+
+
+
+
+
+
+
+            /*
             int totalSegundos, horas, minutos, segundos, resto;
 
             Console.Write("Digite a quantidade de segundos: ");
@@ -27,7 +56,7 @@ namespace Testes
 
             Console.WriteLine($"O total de segundos digitados correspondem a hora {horas}:{minutos}:{segundos}");
 
-
+            */
 
             /*int tipoCha = int.Parse(Console.ReadLine());
             string[] quantidade = Console.ReadLine().Split(' ');
--- Testes/obj/Debug/net8.0/Testes.AssemblyInfo.cs
+++ Testes/obj/Debug/net8.0/Testes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deca0428939781c77b5e54c4fcc4a0071c01e23c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
